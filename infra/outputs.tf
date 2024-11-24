9328eb35d008d2493d8c9374b0d9df71cfb95e04
EC2 Default User <ec2-user@ip-172-31-33-17.eu-west-1.compute.internal>
2024-11-24 03:06:52 +0000

    Added output for SQS CloudWatch alarm

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,14 @@ output "lambda_function_arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.lambda_function.arn
 }
+
+output "cloudwatch_alarm_name" {
+  value       = aws_cloudwatch_metric_alarm.sqs_oldest_message_alarm.alarm_name
+  description = "Name of the CloudWatch alarm for SQS."
+}
+
+output "sns_topic_arn" {
+  value       = aws_sns_topic.alarm_topic.arn
+  description = "ARN of the SNS Topic used for CloudWatch alarm notifications."
+}
+
