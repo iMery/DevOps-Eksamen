d6ad04d17d1f2c8978d22d40844cd7d7c09056eb
EC2 Default User <ec2-user@ip-172-31-33-17.eu-west-1.compute.internal>
2024-11-24 02:12:22 +0000

    Adding alarm variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,29 +1,30 @@
+#Variable that defines the name of the SQS queue 
 variable "sqs_queue_name" {
-  description = "Navn på SQS køen"
+  description = "SQS queue name"
   type        = string
   default     = "maqueue01"
 }
-
+#Variable that specifies the name of the lambda function
 variable "lambda_function_name" {
-  description = "Navn på Lambda-funksjonen"
+  description = "Lambda function name"
   type        = string
   default     = "lambda-function"
 }
-
+#Variable that specifies the name of the S3 bucket
 variable "lambda_s3_bucket" {
-  description = "Navn på S3 bucket for Lambda kode"
+  description = "S3 bucket name"
   type        = string
   default     = "pgr301-couch-explorers"
 }
-
+#This variable is used to specify the path to the lambda functions deployment package
 variable "lambda_s3_key" {
-  description = "S3 nøkkel for Lambda kode"
+  description = "S3 key for lambda"
   type        = string
   default     = "lambda/lambda.zip"
 }
 
-variable "image_s3_bucket" {
-  description = "S3 bucket for genererte bilder"
+#Variable that specifies the email adress that recieves notifications
+variable "alarm_email" {
+  description = "Email address that recives the cloudwatch alarm notifications"
   type        = string
-  default     = "generated-images-bucket"
 }
